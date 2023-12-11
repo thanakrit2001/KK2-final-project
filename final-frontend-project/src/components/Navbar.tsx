84155834baf7d47dfe071f10933135fe5805d058
thanakritcankha <68786794+thanakritcankha@users.noreply.github.com>
2023-12-11 17:03:17 +0700

    not official

---
+++ final-frontend-project/src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+export function Navbar() {
+  return (
+    <>
+      <div className="container">
+        <div className="nav-container">
+          <div className="comp-name">
+            <h1>XiAO</h1>
+          </div>
+          <div className="list-views">
+            <a href="">
+              <h3>Home</h3>
+            </a>
+            <a href="">
+              <h3>Patiants</h3>
+            </a>
+            <a href="">
+              <h3>Dis</h3>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
