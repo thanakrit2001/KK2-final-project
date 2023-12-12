3ebb3c15b5bc3f49efa56143ca2286b04404131d
Preeyathorn Chaiyakum <neko7687@gmail.com>
2023-12-13 00:16:53 +0700

    Flex??

--- reactfrontend/src/views/patients.js
+++ reactfrontend/src/views/patients.js
@@ -88,7 +88,7 @@ function Patients() {
         </table>
       </div>
 
-      <div className="absolute bottom-0">
+      <div className="absolute bottom-5">
         <Popup
           trigger={
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -163,7 +163,7 @@ function Patients() {
                     </div>
                   </div>
                   <div class="flex flex-wrap mx-3 mb-6">
-                    <div class="w-1/4 px-3">
+                    <div class="w-1/3 px-3">
                       <label
                         class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                         for="dateofbirth"
@@ -177,9 +177,6 @@ function Patients() {
                         placeholder="Please select a date"
                         id="dateofbirth"
                       />
-                      <p class="text-gray-600 text-xs italic">
-                        Make it as long and as crazy as you'd like
-                      </p>
                     </div>
                     <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                       <label
@@ -195,9 +192,7 @@ function Patients() {
                         placeholder="(+66)"
                       />
                     </div>
-                  </div>
-                  <div class="flex flex-wrap mx-3 mb-8">                    
-                    <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                       <label
                         class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                         for="gender"
@@ -223,8 +218,9 @@ function Patients() {
                         </div>
                       </div>
                     </div>
-
-                    <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+                  </div>
+                  <div class="flex flex-wrap mx-3 mb-8">
+                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                       <label
                         class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                         for="weight"
@@ -238,7 +234,7 @@ function Patients() {
                         placeholder="kg."
                       />
                     </div>
-                    <div class="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                       <label
                         class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                         for="height"
@@ -252,13 +248,45 @@ function Patients() {
                         placeholder="cm."
                       />
                     </div>
+                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                      <label
+                        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                        for="blood-type"
+                      >
+                        Blood type
+                      </label>
+                      <div class="relative">
+                        <select
+                          class="appearance-none w-full bg-gray-50 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                          id="blood-type"
+                        >
+                          <option>A positive (A+)</option>
+                          <option>A negative (A-)</option>
+                          <option>B positive (B+)</option>
+                          <option>B negative (B-)</option>
+                          <option>AB positive (AB+)</option>
+                          <option>AB positive (AB+)</option>
+                          <option>O positive (O+)</option>
+                          <option>O negative (O-)</option>
+                        </select>
+                        <div class="absolute inset-y-0 right-1 pointer-events-none flex items-center text-gray-700">
+                          <svg
+                            class="fill-current h-4 w-4"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                          >
+                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                          </svg>
+                        </div>
+                      </div>
+                    </div>                    
                   </div>
                 </form>
               </div>
-              <div className="actions">     
-                    <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white mb-4 py-2 px-4 border border-blue-500 hover:border-transparent rounded-lg">                     
-                      Submit
-                    </button>
+              <div className="actions">
+                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white mb-4 py-2 px-4 border border-blue-500 hover:border-transparent rounded-lg">
+                  Submit
+                </button>
               </div>
             </div>
           )}
