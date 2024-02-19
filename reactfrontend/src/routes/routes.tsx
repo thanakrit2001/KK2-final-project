ff59a23f50ad4e827b4b5eb688a7eec314e58eca
thanakritcankha <93530159+thanakrit2001@users.noreply.github.com>
2024-02-19 23:19:23 +0700

    Register

--- reactfrontend/src/routes/routes.tsx
+++ reactfrontend/src/routes/routes.tsx
@@ -20,6 +20,6 @@ export const router = createBrowserRouter([
   },
   {
     path: '/register',
-    element: <Register/>,
+    element: <Register />,
   },
 ])
--- reactfrontend/src/views/Register.tsx
+++ reactfrontend/src/views/Register.tsx
@@ -1,4 +1,40 @@
+import React, { useState } from 'react'
+
 export const Register = () => {
+  const [email, setEmail] = useState('')
+  const [isValid, setIsValid] = useState(true)
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newEmail = event.target.value
+    setEmail(newEmail)
+
+    // ตรวจสอบรูปแบบอีเมล
+    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+    setIsValid(emailPattern.test(newEmail))
+  }
+
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [passwordsMatch, setPasswordsMatch] = useState(true)
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = event.target.value
+    setPassword(newPassword)
+
+    // ตรวจสอบว่า password ตรงกันหรือไม่
+    setPasswordsMatch(newPassword === confirmPassword)
+  }
+
+  const handleConfirmPasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const newConfirmPassword = event.target.value
+    setConfirmPassword(newConfirmPassword)
+
+    // ตรวจสอบว่า password ตรงกันหรือไม่
+    setPasswordsMatch(password === newConfirmPassword)
+  }
+
   return (
     <div className="container min-w-full flex flex-col min-h-screen justify-center items-center">
       <div className="form-container rounded-md shadow-lg w-1/3 p-6 bg-white border-solid border-2">
@@ -34,18 +70,35 @@ export const Register = () => {
               type="email"
               id="email"
               placeholder="example@mail.com"
-              className="w-3/4 mt-2 py-2 px-2 border rounded-md"
+              maxLength={50}
+              className={`w-3/4 mt-2 py-2 px-2 border rounded-md ${
+                isValid ? '' : 'border-red-500'
+              }`}
+              value={email}
+              onChange={handleEmailChange}
             />
+            {!isValid && <p className="text-red-500">Invalid email format.</p>}
+
             <br />
 
             <h3 className="font-bold">Confirm Password:</h3>
-            <p className="text-red-500 text-sm hidden error"></p>
             <input
               type="password"
               id="pass"
               placeholder="Input your password again"
-              className="w-3/4 mt-2 py-2 px-2 border rounded-md"
+              className={`w-3/4 mt-2 py-2 px-2 border rounded-md ${
+                passwordsMatch ? '' : 'border-red-500'
+              }`}
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
             />
+            <p
+              className={`text-red-500 text-sm ${
+                passwordsMatch ? 'hidden' : ''
+              }`}
+            >
+              Password not match.
+            </p>
             <br />
           </div>
 
@@ -56,6 +109,8 @@ export const Register = () => {
               id="pass"
               placeholder="Your password"
               className="w-3/4 mt-2 py-2 px-2 border rounded-md"
+              value={password}
+              onChange={handlePasswordChange}
             />
             <br />
           </div>
